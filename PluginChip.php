2abf3ec264934423ffbeb274f0e411fbd00e4eb7
qir4ni <qiraniawis@gmail.com>
2024-10-03 07:31:42 +0000

    Initialize Project

---
+++ PluginChip.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'modules/admin/models/GatewayPlugin.php';
+require_once 'modules/billing/models/class.gateway.plugin.php';
+
+/**
+ * @package Plugins
+ */
+class PluginChip extends GatewayPlugin
+{
+    
+}
---
+++ PluginChipCallback.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'modules/admin/models/PluginCallback.php';
+require_once 'modules/billing/models/class.gateway.plugin.php';
+
+class PluginChipCallback extends PluginCallback
+{
+
+}
+
