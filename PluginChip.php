a69d3dbb7349d8b6dc81b7d8b0a649e0ef08d048
qir4ni <qiraniawis@gmail.com>
2024-10-16 04:39:41 +0000

    Testing for Autopayment

--- PluginChip.php
+++ PluginChip.php
@@ -30,7 +30,22 @@ class PluginChip extends GatewayPlugin
                 "type"          => "text",
                 "description"   => lang("Select the name to display in the signup process for this payment type. Example: Mollie iDeal."),
                 "value"         => "CHIP"
-            )
+            ),
+            lang('Auto Payment') => array(
+                'type'        => 'hidden',
+                'description' => lang('No description'),
+                'value'       => '1'
+            ),
+            lang('CC Stored Outside') => array(
+                'type'        => 'hidden',
+                'description' => lang('If this plugin is Auto Payment, is Credit Card stored outside of Clientexec? 1 = YES, 0 = NO'),
+                'value'       => '1'
+            ),
+            lang('Billing Profile ID') => array(
+                'type'        => 'hidden',
+                'description' => lang('Is this plugin storing a Billing-Profile-ID? 1 = YES, 0 = NO'),
+                'value'       => '1'
+            ),
         );
 
         return $variables;
@@ -38,7 +53,29 @@ class PluginChip extends GatewayPlugin
 
     public function singlePayment($params)
     {
-        CE_Lib::log(4, 'SinglePayment() initiated');
+        // CE_Lib::log(4, $params);
+        // die();
+        return $this->autopayment($params);
+    }
+
+    public function credit($params) 
+    {
+        // CE_Lib::log(4, $this->settings->get('plugin_chip_Brand ID'));
+        // die();
+        // $params['refund'] = true;
+        // return $this->singlePayment($params);
+
+        // $user = new User($params['CustomerID']);
+        // $this->getBillingProfileID($user);
+        die();
+    }
+
+    public function autopayment($params) 
+    {
+        // Check for autopayment
+        CE_Lib::log(4, 'AutoPayment() initiated');
+
+        // die();
 
         // Set the credentials
         $brandId = $params['plugin_chip_Brand ID'];
@@ -70,13 +107,30 @@ class PluginChip extends GatewayPlugin
             exit();
         }
 
+        // Create plug in class to interact with CE.
+        $cPlugin = new Plugin($params['invoiceNumber'], 'chip', $this->user);
+        $cPlugin->setAmount($invoiceTotal);
+
+        try {
+            // User
+            $user = new User($params['CustomerID']);
+            CE_Lib::log(4, 'Create user!');
+            CE_Lib::log(4, $user);
+            $this->getBillingProfileID($user);
+           
+            die();
+
+        } catch (Exception $e) {
+            CE_Lib::log(1, $this->user->lang("There was an error performing this operation.")." ".$e->getMessage());
+
+            $cPlugin->PaymentRejected($this->user->lang("There was an error performing this operation.")." ".$e->getMessage());
+            return $this->user->lang("There was an error performing this operation.")." ".$e->getMessage();
+        }
+
+        die();
+
         // Checking for refund
         if (isset($params['refund']) && $params['refund']) {
-            CE_Lib::log(4, 'Refund running in singlePayment()');
-
-            // Create plug in class to interact with CE.
-            $cPlugin = new Plugin($params['invoiceNumber'], 'chip', $this->user);
-            $cPlugin->setAmount($invoiceTotal);
             $cPlugin->setAction('refund');
 
             // Get transid
@@ -142,12 +196,108 @@ class PluginChip extends GatewayPlugin
         exit();
     }
 
-    public function credit($params) 
+    // Get customer CHIP Profile
+    public function getBillingProfileID($user)
     {
-        CE_Lib::log(4, 'Refund in credit() initiated');
+        CE_Lib::log(4, 'In getBillingProfileID()');
+        CE_Lib::log(4,  $user);
+        
+        die();
+        // test purpose
+        $secretKey = $this->settings->get('plugin_chip_API Key');
+        $brandId = $this->settings->get('plugin_chip_Brand ID');
 
-        // Refund is true
-        $params['refund'] = true;
-        return $this->singlePayment($params);
+        $profile_id = '';
+        $payment_method = '';
+        $Billing_Profile_ID = '';
+
+        $secretKey = $this->settings->get('plugin_chip_API Key');
+        $brandId = $this->settings->get('plugin_chip_Brand ID');
+
+        CE_Lib::log(4, "Billig-Profile-ID");
+        CE_Lib::log(4, $user->getCustomFieldsValue('Billing-Profile-ID', $Billing_Profile_ID));
+        // die();
+
+        if ($user->getCustomFieldsValue('Billing-Profile-ID', $Billing_Profile_ID) && $Billing_Profile_ID != '') {
+            $profile_id_array = unserialize($Billing_Profile_ID);
+            CE_Lib::log(4, "Displaying Profile ID Array");
+            CE_Lib::log(4, $profile_id_array);
+            
+            if (is_array($profile_id_array) && isset($profile_id_array[basename(dirname(__FILE__))])) {
+                $profile_id = $profile_id_array[basename(dirname(__FILE__))];
+            }
+        }
+
+        CE_Lib::log(4, "Displaying Profile ID");
+        CE_Lib::log(4, $profile_id);
+
+        $profile_id_values_array = explode('|', $profile_id);
+        $profile_id = $profile_id_values_array[0];
+
+        if (isset($profile_id_values_array[1])) {
+            $payment_method = $profile_id_values_array[1];
+            CE_Lib::log(4, $payment_method);
+        } else {
+            CE_Lib::log(4, 'Else profile id value not set');
+            if ($profile_id != '') {
+                try {
+                    // Get the client 
+                    $chip   = Chip::get_instance($secretKey, $brandId);
+                    $customers_api = $chip->get_client_by_email($user->fields['email'])['results'][0];
+
+                    try {
+                        // $result = $customers_api->retrieveCustomer($profile_id);
+
+
+                        // Retrieve list of recurring tokens of clients
+                        $recurring_tokens = $chip->list_tokens($customers_api['id'])['results'];
+                        CE_Lib::log(4, $recurring_tokens);
+                        die();
+
+                    } catch (Exception $e) {
+                        $profile_id = '';
+                    }
+                } catch (Exception $e) {
+                    $profile_id = '';
+                }
+            } else {
+                CE_Lib::log(4, 'Profile ID is empty');
+            }
+        }
+
+        $profile_id_values_array[0] = $profile_id;
+        $profile_id_values_array[1] = $payment_method;
+
+        // $this->setBillingProfileID($user, $profile_id_values_array);
+
+        // return $profile_id_values_array;
+
+        CE_Lib::log(4, 'End of getBillingProfileID()');
+    }
+
+    // set Billing-Profile-ID
+    public function setBillingProfileID($user, $profile_id_values_array)
+    {
+        $profile_id = $profile_id_values_array[0];
+        $payment_method = $profile_id_values_array[1];
+        $Billing_Profile_ID = '';
+        $profile_id_array = array();
+
+        if ($user->getCustomFieldsValue('Billing-Profile-ID', $Billing_Profile_ID) && $Billing_Profile_ID != '') {
+            $profile_id_array = unserialize($Billing_Profile_ID);
+        }
+
+        if (!is_array($profile_id_array)) {
+            $profile_id_array = array();
+        }
+
+        if ($profile_id != '' && $payment_method != '') {
+            $profile_id_array[basename(dirname(__FILE__))] = $profile_id.'|'.$payment_method;
+        } else {
+            unset($profile_id_array[basename(dirname(__FILE__))]);
+        }
+
+        $user->updateCustomTag('Billing-Profile-ID', serialize($profile_id_array));
+        $user->save();
     }
 }
